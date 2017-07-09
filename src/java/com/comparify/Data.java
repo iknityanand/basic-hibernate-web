d9502bfe1632fc6eecfa860a39263a6004585cd4
Nityanand Katare <iknityanand@gmail.com>
2017-07-09 13:07:22 +0530

    insert in db

---
+++ src/java/com/comparify/Data.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.comparify;
+
+import javax.annotation.ManagedBean;
+import org.hibernate.Session;
+
+/**
+ *
+ * @author Vineet
+ */
+@ManagedBean
+public class Data {
+    private Userdetails u;
+    private HibernateUtil helper;
+    private Session session;
+    
+    public void addUser(){
+       u=new Userdetails("Nityanand");
+       session =helper.getSessionFactory().openSession();
+       session.beginTransaction();
+       session.save(u);
+       session.getTransaction().commit();
+       session.close();               
+    }
+    
+}
---
+++ src/java/com/comparify/Userdetails.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.comparify;
+
+/**
+ *
+ * @author Vineet
+ */
+public class Userdetails {
+    private int id;
+    private String name;
+
+    public Userdetails(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+    
+    
+}
